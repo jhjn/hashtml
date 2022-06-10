50da77d14c97d375cf5323c8181eca415a6de7b7
Joe Jenne <joe@jhjn.xyz>
2022-06-10 13:39:26 +0100

    Obfuscate

--- hashtml.js
+++ hashtml.js
@@ -1,28 +1,3 @@
-const encrypt = (salt, text) => {
-  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
-  const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
-  const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
-
-  return text
-    .split("")
-    .map(textToChars)
-    .map(applySaltToChar)
-    .map(byteHex)
-    .join("");
-};
-
-const decrypt = (salt, encoded) => {
-  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
-  const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
-  return encoded
-    .match(/.{1,2}/g)
-    .map((hex) => parseInt(hex, 16))
-    .map(applySaltToChar)
-    .map((charCode) => String.fromCharCode(charCode))
-    .join("");
-};
-
-const key = window.location.hash.slice(1);
-const encrypted = document.getElementById("encrypted").innerHTML;
-document.getElementById("decrypted").innerHTML = decrypt(key,encrypted);
+const k = window.location.hash.slice(1).split("").map((c) => c.charCodeAt(0));
+document.getElementById("decrypted").innerHTML=document.getElementById("encrypted").innerHTML.match(/.{1,2}/g).map((h)=>parseInt(h,16)).map((v)=>k.reduce((a,b)=>a^b,v)).map((c)=>String.fromCharCode(c)).join("");
 
